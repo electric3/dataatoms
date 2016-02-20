7507fd2a93fe9ab2c9107e87fa5a3e25022e705a
Stanislav Smirnov <stas.smirnov88@gmail.com>
2016-02-20 13:41:33 +0300

    gsonable, fix methods, removing extra

--- src/main/java/com/electric3/dataatoms/Gsonable.java
+++ src/main/java/com/electric3/dataatoms/Gsonable.java
@@ -2,22 +2,14 @@ package com.electric3.dataatoms;
 
 import com.google.gson.Gson;
 
-import java.lang.reflect.Type;
+public class Gsonable {
+    private static final Gson gson = new Gson();
 
-public class Gsonable  {
     public String serialize() {
-        return new Gson().toJson(this);
+        return gson.toJson(this);
     }
 
-    public static Object deserialize(Class clazz, String str) {
-        return new Gson().fromJson(str, clazz);
-    }
-
-    public static Object deserialize(Type type, String str) {
-        return new Gson().fromJson(str, type);
-    }
-
-    public <T extends Gsonable> T deserialize(String string, Class<T> tClass) {
-        return new Gson().fromJson(string, tClass);
+    public static <T extends Gsonable> T deserialize(String string, Class<T> tClass) {
+        return gson.fromJson(string, tClass);
     }
 }
